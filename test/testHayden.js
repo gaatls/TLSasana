5ff0ce99339526ddf52206c1c85e3b74fb8c6993
Hayden Wagner (Student Employee) <tlshbw@ad.rit.edu>
2016-09-09 14:52:23 -0400

    Adds explanation haydenTest file, (only committing to project b/c I switch desktops a lot)

--- test/testHayden.js
+++ test/testHayden.js
@@ -1,5 +1,7 @@
-/**This is a development only file that I was using to clean up the test file and make the 
- * main test file more organized
+/**This is a development only file that I am using to make initial tests and 
+ * clean up existing test without running the whole test file everytime
+ *
+ * ONLY COMMITTING BECAUSE I SWITCH COMPUTERS A LOT...otherwise I would ignore this file
  */
 
 
@@ -7,13 +9,16 @@
 var assert = require('assert');
 var tlsAsana = require('../index.js');
 var tlsConsts = require('../tlsConstants.js')
+let client = undefined;
 
 //// CONNECTION -----------------------------------
 describe('Connecting to Asana', function(){
    it('Should setup the connection', function(){
         this.timeout(8000);
         return tlsAsana.connect('166216691534199').then(function(response){
-            assert.deepEqual(response,true,'Successful connection');
+            client = response;
+            //console.log(client);
+            assert(response,'Connection failed');
         });
    });
 });
@@ -24,31 +29,31 @@ describe('Working with tags in Asana', function(){
 
     it('changes a tag from captioning_unassigned to captioning_accepted', function(){
         this.timeout(8000);
-        let promiseArray = [
-            tlsAsana.getTagIDByName('captioning_unassigned'),
-            tlsAsana.getTagIDByName('captioning_accepted')
-        ];
-
-        return Promise.all(promiseArray).then(responses => {
-            return tlsAsana.switchTag('166304358745259', responses[0], responses[1]).then(tag => {
-                assert.deepEqual(tag, {}, 'tag change failed');
-            });
+        
+        return tlsAsana.switchTag('166304358745259', 'captioning_unassigned', 'captioning_accepted').then(tag => {
+                return client.tasks.findById('166304358745259').then(taskInfo => {  
+                    //Test looks at the tag in the first position of the tag array for this 
+                    //task,so the test may be broken if other tags are added to this task
+                    console.log('Task tag switched to:');
+                    console.log(taskInfo.tags[0]);
+                    assert.strictEqual(taskInfo.tags[0].name, 'captioning_accepted', 'tag change failed');
+                });
         });
     });
 
-    // it('changes a tag back from captioning_accepted to captioning_unassigned ', function(){
-    //     this.timeout(8000);
-    //     let promiseArray = [
-    //         tlsAsana.getTagIDByName('captioning_accepted'),
-    //         tlsAsana.getTagIDByName('captioning_unassigned')
-    //     ];
-
-    //     return Promise.all(promiseArray).then(responses => {
-    //         return tlsAsana.switchTag('166304358745259', responses[0], responses[1]).then(function(tag){
-    //             assert(tag, 'tag change failed');
-    //         });
-    //     });
-    // });
+    it('changes a tag from captioning_accepted to captioning_unassigned', function(){
+        this.timeout(8000);
+        
+        return tlsAsana.switchTag('166304358745259', 'captioning_accepted', 'captioning_unassigned').then(tag => {
+                return client.tasks.findById('166304358745259').then(taskInfo => {  
+                    //Test looks at the tag in the first position of the tag array for this 
+                    //task,so the test may be broken if other tags are added to this task
+                    console.log('Task tag switched to:');
+                    console.log(taskInfo.tags[0]);
+                    assert.strictEqual(taskInfo.tags[0].name, 'captioning_unassigned', 'tag change failed');
+                });
+        });
+    });
 
 });
 
